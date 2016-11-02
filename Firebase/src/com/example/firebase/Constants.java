d61e63b3db1f9d0fcfa07ba4b55fdb9ea881fb62
gyanprakash93 <gnprks@gmail.com>
2016-11-02 07:24:23 +0530

    Update Constants.java

--- Firebase/src/com/example/firebase/Constants.java
+++ Firebase/src/com/example/firebase/Constants.java
@@ -1,5 +1,5 @@
 package com.example.firebase;
 
 public class Constants {
-	 public static final String FIREBASE_URL = "https://ivepos-online.firebaseio.com/"; // Replace with your own URL
+	 public static final String FIREBASE_URL = "https://your-project-name.firebaseio.com/"; // Replace with your own URL
 }
